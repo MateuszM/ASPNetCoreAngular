13c3667d75878d3378bdcee4ebc24c70a6764850
AnotherLie <lordring66@gmail.com>
2021-06-10 09:49:50 +0200

    More to login controller

---
+++ InfrastructureLayer/Data/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Data
+{
+    public interface IUserRepository<T>
+    {
+        Task InsertEntity(string role, string id, string fullName);
+        Task InsertUser(T user);
+
+    }
+}
--- NetCoreAngularShop/Controllers/LoginController.cs
+++ NetCoreAngularShop/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
 ﻿using IdentityServer4.Events;
 using IdentityServer4.Services;
 using InfrastructureLayer.Model;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NetCoreAngularShop.Models;
 using NetCoreAngularShop.Models.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -20,7 +22,7 @@ namespace NetCoreAngularShop.Controllers
         {
             return View();
         }
-        public LoginController(UserManager<AppUser> userManager,IEventService events, IIdentityServerInteractionService interaction)
+        public LoginController(UserManager<AppUser> userManager, IEventService events, IIdentityServerInteractionService interaction)
         {
             this.userManager = userManager;
             this.events = events;
@@ -29,17 +31,57 @@ namespace NetCoreAngularShop.Controllers
         public async Task<IActionResult> Login(LoginInputViewModel model)
         {
             var context = interaction.GetAuthorizationContextAsync(model.ReturnUrl);
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var user = await userManager.FindByNameAsync(model.UserName);
-               
-                if(user!=null && await userManager.CheckPasswordAsync(user,model.Password))
+
+                if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
                 {
-                   await events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.FirstName, user.LastName));
+                    await events.RaiseAsync(new UserLoginSuccessEvent(user.UserName, user.Id, user.FirstName, user.LastName));
+                    AuthenticationProperties props = null;
+                    if (AccountOptions.AllowRememberLogin && model.RememberLogin)
+                    {
+                        props = new AuthenticationProperties
+                        {
+                            IsPersistent = true,
+                            ExpiresUtc = DateTimeOffset.UtcNow.Add(AccountOptions.RememberMeLoginDuration)
+                        };
+                    };
+                    await HttpContext.SignInAsync(user.Id, user.UserName, props);
+
+                    if (context != null)
+                    {
+                        // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
+                        return Redirect(model.ReturnUrl);
+                    }
+                    // request for a local page
+                    if (Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else if (string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return Redirect("~/");
+                    }
+                    else
+                    {
+                        // user might have clicked on a malicious link - should be logged
+                        throw new Exception("invalid return URL");
+                    }
                 }
+                await events.RaiseAsync(new UserLoginFailureEvent(model.UserName, "invalid credentials"));
+                ModelState.AddModelError(string.Empty, AccountOptions.InvalidCredentialsErrorMessage);
             }
             var vm = new LoginInputViewModel() { UserName = model.UserName, RememberLogin = model.RememberLogin };
             return View(vm);
         }
+        private async Task GetClaims(AppUser user)
+        {
+            var claimsPrincipal = await userManager.crea
+            var claims = claimsPrincipal.Claims.ToList();
+        }
+        
+
+        
     }
 }
