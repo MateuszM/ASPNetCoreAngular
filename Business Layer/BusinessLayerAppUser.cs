98ef2bf03536927b8d681429588aeb1b5b63aa85
AnotherLie <lordring66@gmail.com>
2021-06-15 20:05:07 +0200

    SomeChanges

---
+++ Business Layer/BusinessLayerAppUser.cs
@@ -0,0 +1,21 @@
+﻿using InfrastructureLayer.Data;
+using InfrastructureLayer.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+    public class BusinessLayerAppUser
+    {
+        private AppIdentityDbContext dbContext;
+        public BusinessLayerAppUser(AppIdentityDbContext context)
+        {
+            dbContext = context;
+        }
+        public void SaveUser(AppUser user)
+        {
+            dbContext.Add(user);
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Business Layer/BusinessLayerClaims.cs
@@ -0,0 +1,25 @@
+﻿using InfrastructureLayer.Data;
+using InfrastructureLayer.Model;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer
+{
+   public class BusinessLayerClaims
+    {
+        private AppIdentityDbContext context;
+        public BusinessLayerClaims(AppIdentityDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public List<IdentityUserClaim<string> >LoadUserClaims(AppUser user)
+        {
+            return context.UserClaims.Where(x => x.UserId == user.Id).ToList();
+        }
+    }
+}
